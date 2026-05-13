a20c663dd2d7a21cd19dc8d590b3ce161349f71a
ddurumboy2197 <ddurumboy2197@gmail.com>
2026-05-13 16:21:41 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,6 @@
+def olib_tashlash(ro'yxat):
+    return list(dict.fromkeys(ro'yxat))
+
+# Misol:
+ro'yxat = [1, 2, 2, 3, 4, 4, 5, 6, 6]
+print(olib_tashlash(ro'yxat))  # Chiqaradi: [1, 2, 3, 4, 5, 6]
